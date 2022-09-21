80bbce4528c5e36ed6c6ad97e8c85d480fb8f9d1
Mangdi <lkw4670@naver.com>
2022-09-21 22:20:32 +0900

    api로 자료를 받아와서 분석하기

--- Doit_R_Shiny/03_자료수집.R
+++ Doit_R_Shiny/03_자료수집.R
@@ -2,3 +2,93 @@ install.packages("rstudioapi")
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 getwd()
 
+loc <- read.csv("../Open_data_R_with_Shiny-main/01_code/sigun_code/sigun_code.csv", fileEncoding="UTF-8")  #  지역코드
+loc$code <- as.character(loc$code) # 행정구역명 문자 변환     
+head(loc, 2) # 확인
+
+datelist <- seq(from = as.Date('2021-01-01'), # 시작
+                to   = as.Date('2021-12-31'), # 종료
+                by    = '1 month')            # 단위
+datelist <- format(datelist, format = '%Y%m') # 형식변환(YYYY-MM-DD => YYYYMM) 
+datelist[1:3]          # 확인
+
+
+
+
+
+service_key <- "NXFeYDCwqS2TqjLyiDMMPTuuQsI1nj6y4Tg4LUWZ5hhXzAqWOvS85SGpbpOG0xxj%2Fw9OSrEn8vdnGGt%2B67phKw%3D%3D"
+url_list <- list()
+cnt <- 0
+
+for(i in 1:nrow(loc)){           # 외부반복: 25개 자치구
+  for(j in 1:length(datelist)){  # 내부반복: 12개월
+    cnt <- cnt + 1               # 반복누적 카운팅
+    #---# 요청 목록 채우기 (25 X 12= 300)
+    url_list[cnt] <- paste0("http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?",
+                            "LAWD_CD=", loc[i,1],         # 지역코드
+                            "&DEAL_YMD=", datelist[j],    # 수집월
+                            "&numOfRows=", 100,           # 한번에 가져올 최대 자료 수
+                            "&serviceKey=", service_key)  # 인증키
+  } 
+  Sys.sleep(0.1)   # 0.1초간 멈춤
+  msg <- paste0("[", i,"/",nrow(loc), "]  ", loc[i,3], " 의 크롤링 목록이 생성됨 => 총 [", cnt,"] 건") # 알림 메시지
+  cat(msg, "\n\n") 
+}
+
+length(url_list)
+browseURL(paste0(url_list[1]))
+
+
+
+
+
+library(XML)        # install.packages("XML")      
+library(data.table) # install.packages("data.table")
+library(stringr)    # install.packages("stringr")
+
+raw_data <- list()        # xml 임시 저장소
+root_Node <- list()       # 거래내역 추출 임시 저장소
+total <- list()           # 거래내역 정리 임시 저장소
+dir.create("02_raw_data") # 새로운 폴더 만들기
+
+for(i in 1:length(url_list)){   # 요청목록(url_list) 반복
+  raw_data[[i]] <- xmlTreeParse(url_list[i], useInternalNodes = TRUE,encoding = "utf-8") # 결과 저장
+  root_Node[[i]] <- xmlRoot(raw_data[[i]])	# xmlRoot로 추출
+  
+  
+  items <- root_Node[[i]][[2]][['items']]  # 전체 거래내역(items) 추출
+  size <- xmlSize(items)                   # 전체 거래 건수 확인    
+  
+  
+  
+  
+  
+  item <- list()  # 전체 거래내역(items) 저장 임시 리스트 생성
+  item_temp_dt <- data.table()  # 세부 거래내역(item) 저장 임시 테이블 생성
+  Sys.sleep(.1)  # 0.1초 멈춤
+  for(m in 1:size){  # 전체 거래건수(size)만큼 반복
+    #---# 세부 거래내역 분리   
+    item_temp <- xmlSApply(items[[m]],xmlValue)
+    item_temp_dt <- data.table(year = item_temp[4],     # 거래 년 
+                               month = item_temp[7],    # 거래 월
+                               day = item_temp[8],      # 거래 일
+                               price = item_temp[1],    # 거래금액
+                               code = item_temp[12],    # 지역코드
+                               dong_nm = item_temp[5],  # 법정동
+                               jibun = item_temp[11],   # 지번
+                               con_year = item_temp[3], # 건축연도 
+                               apt_nm = item_temp[6],   # 아파트 이름   
+                               area = item_temp[9],     # 전용면적
+                               floor = item_temp[13])   # 층수 
+    item[[m]] <- item_temp_dt}    # 분리된 거래내역 순서대로 저장
+  apt_bind <- rbindlist(item)     # 통합 저장
+  
+  
+  
+  
+  region_nm <- subset(loc, code== str_sub(url_list[i],115, 119))$addr_1 # 지역명 추출
+  month <- str_sub(url_list[i],130, 135)   # 연월(YYYYMM) 추출
+  path <- as.character(paste0("./02_raw_data/", region_nm, "_", month,".csv")) # 저장위치 설정
+  write.csv(apt_bind, path)     # csv 저장
+  msg <- paste0("[", i,"/",length(url_list), "] 수집한 데이터를 [", path,"]에 저장 합니다.") # 알림 메시지
+  cat(msg, "\n\n")
